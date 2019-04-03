cbe6f03c9aabc11b6c96ba4d0b150882eb1eb2f1
Seo <32647144+alovena@users.noreply.github.com>
2019-04-03 14:53:59 +0900

    Update 퀵정렬.c

--- 퀵정렬.c
+++ 퀵정렬.c
@@ -1,33 +1,35 @@
 #include<stdio.h>
 int arr[8] = { 5,4,3,1,6,7,8,2 };
 void partition(int low, int high, int *pp) {
-	*pp = arr[low];
+	int item = arr[low];
+	//*pp = low;
+
 	int j = low;
 	int temp;
 
 	for (int i = low + 1; i <= high; i++) {
-		if (*pp > arr[i]) {
+		if (item > arr[i]) {
 			j++;
 			temp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp;
+
 		}
 	}
 	*pp = j;
-	if (*pp == 0) {
-		return 0;
-	}
-	//printf("%d", *pp);
+
 	temp = arr[low];
 	arr[low] = arr[*pp];
 	arr[*pp] = temp;
-
 }
 void Quicksort(int low, int high) {
 	int pp;
 	if (low < high) {
 		partition(0, 7, &pp);
-		
+
+		if (pp == 0) {
+			return 0;
+		}
 		Quicksort(0, (pp)-1);
 		Quicksort((pp)+1, 7);
 	}
@@ -36,8 +38,8 @@ void Quicksort(int low, int high) {
 int main(void) {
 
 	Quicksort(0, 7);
-	for (int i = 0; i < 8; i++) {
-		printf("%d" , arr[i]);
+	for (int i = 0; i < 7; i++) {
+		printf("%d", arr[i]);
 	}
 	return 0;
 }
