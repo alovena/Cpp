509ae791bc375e24d50865d92f8eeb37f451efb6
Seo <32647144+alovena@users.noreply.github.com>
2019-04-09 20:48:20 +0900

    Update 선택정렬.py

--- 선택정렬.py
+++ 선택정렬.py
@@ -1,16 +1,16 @@
-﻿def find_smallest(arr):
-    smallest =arr[0] #최소값 초기화
-    smallest_index=0# 최소값 인덱스 초기화
+﻿def find_smallest(arr):#최소값 찾는 함수
+    smallest =arr[0] 
+    smallest_index=0
     for i in range(1,len(arr)):
-        if arr[i]< smallest:# 최소값을 찾으면
-            smallest=arr[i] #최소값 설정
-            smallest_index =i #최소값에 해당하는 인덱스
-    return smallest_index  # 최소값에 해당되는 인덱스를 반환
+        if arr[i]< smallest:
+            smallest=arr[i] 
+            smallest_index =i 
+    return smallest_index 
 def selection_sort(arr):# arr : 배열을 매개변수로 입력 받음
-    newArr=[] #새로운 배열 생성
+    newArr=[]
     for i in range(len(arr)): #0,len(arr)-1 만큼 반복합니다
-        smallest =find_smallest(arr) #최소값에 해당되는 인덱스을 찾습니다.
-        newArr.append(arr.pop(smallest))#최소값을 배열에 넣습니다.
-    return newArr # 선택정렬된 배열을 반환
+        smallest =find_smallest(arr) 
+        newArr.append(arr.pop(smallest))
+    return newArr 
 myArr=[5,3,6,2,10]
 selection_sort(myArr)
